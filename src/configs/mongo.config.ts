7a2f51ef3c28edc853b4d637f9c920048716b765
Spasidar Hadzhipasev <shadjipasev@gmail.com>
2025-05-02 20:36:31 +0300

    feat: Application containerized with docker.

--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectMongo = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI as string);
+    await mongoose.connect(
+      (process.env.MONGO_URI as string) || "mongodb://mongodb:27017/chatbot-nw"
+    );
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
