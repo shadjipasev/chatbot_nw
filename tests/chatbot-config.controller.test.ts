a398d66dca3a24d2829e77151f8fd145b00a92a3
Spasidar Hadzhipasev <shadjipasev@gmail.com>
2025-05-03 16:43:34 +0300

    feat: chatbot-config.controller.test for route GET/config works properly.

--- tests/chatbot-config.controller.test.ts
+++ tests/chatbot-config.controller.test.ts
@@ -1,33 +1,55 @@
 import { IBlock, IChatConfig } from "src/models/types/config.interface";
 import * as request from "supertest";
 import { createApp } from "../src/server";
-import { createChatConfig } from "../src/services/chatbot-config.service";
+import {
+  createChatConfig,
+  getMostRecentConfig,
+} from "../src/services/chatbot-config.service";
 
 jest.mock("../src/services/chatbot-config.service");
 
 describe("Chatbot Config Controller", () => {
   //   jest.setTimeout(60000);
 
-  it("POST /config should create new configuration", async () => {
-    const mockConfig: IChatConfig = {
-      blocks: <IBlock[]>[
-        {
-          id: "start_block",
-          type: "write_message",
-          content: "Hello!",
-          next: "wait_for_response",
-        },
-      ],
-      startBlock: "start_block",
-    };
-    (createChatConfig as jest.Mock).mockResolvedValue(mockConfig);
-    const app = await createApp();
-    const response = await request(app).post("/config").send(mockConfig);
+  const mockConfig: IChatConfig = {
+    blocks: <IBlock[]>[
+      {
+        id: "start_block",
+        type: "write_message",
+        content: "Hello!",
+        next: "wait_for_response",
+      },
+    ],
+    startBlock: "start_block",
+  };
 
-    expect(response.status).toBe(201);
-    expect(response.body).toEqual({
-      message: "Configuration was successfully uploaded.",
-      data: mockConfig,
+  describe("POST /config", () => {
+    it("should create new configuration", async () => {
+      const app = await createApp();
+
+      (createChatConfig as jest.Mock).mockResolvedValue(mockConfig);
+      const response = await request(app).post("/config").send(mockConfig);
+
+      expect(response.status).toBe(201);
+      expect(response.body).toEqual({
+        message: "Configuration was successfully uploaded.",
+        data: mockConfig,
+      });
+    });
+  });
+
+  describe("GET /config", () => {
+    it("should retrieve latest chatbot configuration.", async () => {
+      const app = await createApp();
+
+      (getMostRecentConfig as jest.Mock).mockResolvedValue(mockConfig);
+      const response = await request(app).get("/config");
+
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual({
+        message: "Retrieving latest chatbot configuration.",
+        data: mockConfig,
+      });
     });
   });
 });
