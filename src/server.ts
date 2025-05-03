a56f85ee0f1f51959130d27a243aabffe71ccd65
Spasidar Hadzhipasev <shadjipasev@gmail.com>
2025-05-03 12:48:49 +0300

    feat: Wrapped server and services initialization in asynch function.

--- src/server.ts
+++ src/server.ts
@@ -6,50 +6,61 @@ import { handleSocketConnection } from "./controllers/socket.controller";
 import { chatConfigRoutes } from "./routes/chat-config.routes";
 import OpenAI from "openai";
 import { historyRoutes } from "./routes/history.route";
-import { setConfig } from "./services/flow.service";
+import { getConfig, setConfig } from "./services/flow.service";
 import { errorHandler } from "./common/error.handler";
-
-try {
-} catch (error) {}
+import { InternalServerError } from "restify-errors";
 
 dotenv.config();
-connectMongo();
 
-const respond = (
-  req: restify.Request,
-  res: restify.Response,
-  next: restify.Next
-) => {
-  res.send("Server started!");
-  next();
-};
+try {
+  start();
+} catch (error) {
+  throw new InternalServerError({
+    message: "Failed to initialize server!",
+    cause: error,
+  });
+}
 
-export const server = restify.createServer();
-setConfig();
-chatConfigRoutes(server);
-historyRoutes(server);
+async function start() {
+  const server = restify.createServer();
 
-server.use(restify.plugins.bodyParser());
-server.on("uncaughtException", errorHandler);
+  await connectMongo();
+
+  const respond = (
+    req: restify.Request,
+    res: restify.Response,
+    next: restify.Next
+  ) => {
+    res.send("Server started!");
+    next();
+  };
+
+  server.get("/", respond);
+  server.head("/", respond);
+  chatConfigRoutes(server);
+  historyRoutes(server);
+
+  const io = new SocketIoServer(server.server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
+  });
+
+  await setConfig();
+
+  handleSocketConnection(io);
+
+  server.use(restify.plugins.bodyParser());
+  server.use(restify.plugins.queryParser());
+  server.on("uncaughtException", errorHandler);
+
+  server.listen(3000, () => {
+    console.log("%s listening at %s", server.name, server.url);
+  });
+}
 
 export const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY as string,
-});
-
-const io = new SocketIoServer(server.server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
-
-handleSocketConnection(io);
-
-server.use(restify.plugins.bodyParser());
-server.get("/", respond);
-server.head("/", respond);
-
-server.listen(3000, () => {
-  console.log("%s listening at %s", server.name, server.url);
 });
