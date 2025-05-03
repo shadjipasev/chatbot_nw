a6fc2e08e103605eb61c4a69e39b746440b9024a
Spasidar Hadzhipasev <shadjipasev@gmail.com>
2025-05-03 11:03:18 +0300

    feat: socket.controller.ts error handling improvement

--- src/controllers/socket.controller.ts
+++ src/controllers/socket.controller.ts
@@ -2,15 +2,23 @@ import { Server, Socket } from "socket.io";
 import { sendInitialMessage, setConfig } from "../services/flow.service";
 import { flowController } from "./flow.controller";
 import { setActiveSocket } from "../services/socket.service";
+import { InternalServerError } from "restify-errors";
 
 export const handleSocketConnection = (io: Server) => {
-  io.on("connection", async (socket: Socket) => {
-    console.log("Connected socket", socket.id);
-    setActiveSocket(socket);
-    sendInitialMessage();
-    socket.on("message_from_client", async (data) => {
-      await flowController(data);
-      console.log("message_from_client", data);
+  try {
+    io.on("connection", async (socket: Socket) => {
+      console.log("Connected socket", socket.id);
+      setActiveSocket(socket);
+      sendInitialMessage();
+      socket.on("message_from_client", async (data) => {
+        await flowController(data);
+        console.log("message_from_client", data);
+      });
     });
-  });
+  } catch (error) {
+    throw new InternalServerError({
+      message: "Socket connection problem.",
+      cause: error,
+    });
+  }
 };
