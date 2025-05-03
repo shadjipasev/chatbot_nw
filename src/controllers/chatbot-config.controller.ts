32f8e1a1f61c3bab7881eadbcddcb8c8b7b1c452
Spasidar Hadzhipasev <shadjipasev@gmail.com>
2025-05-03 10:54:51 +0300

    feat: chatbot-config.controller error handling.

--- src/controllers/chatbot-config.controller.ts
+++ src/controllers/chatbot-config.controller.ts
@@ -3,22 +3,36 @@ import {
   createChatConfig,
   getMostRecentConfig,
 } from "../services/chatbot-config.service";
+import { BadRequestError } from "restify-errors";
 
 export const createConfig = async (req: Request, res: Response) => {
   const jsonCongif = req.body;
-  const createdConfif = await createChatConfig(jsonCongif);
+  try {
+    const createdConfif = await createChatConfig(jsonCongif);
 
-  res.json({
-    message: "Configuration was successfully uploaded.",
-    data: createdConfif,
-  });
+    res.json({
+      message: "Configuration was successfully uploaded.",
+      data: createdConfif,
+    });
+  } catch (error) {
+    throw new BadRequestError({
+      messsage: "Unable to upload configuration",
+      cause: error,
+    });
+  }
 };
 
 export const getLatestConfig = async (req: Request, res: Response) => {
-  const latestConfig = await getMostRecentConfig();
-
-  res.json({
-    message: "Retrieving latest chatbot configuration.",
-    data: latestConfig,
-  });
+  try {
+    const latestConfig = await getMostRecentConfig();
+    res.json({
+      message: "Retrieving latest chatbot configuration.",
+      data: latestConfig,
+    });
+  } catch (error) {
+    throw new BadRequestError({
+      message: "Unable to get configuration",
+      cause: error,
+    });
+  }
 };
