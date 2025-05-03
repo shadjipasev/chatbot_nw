9e6d10668961e8e6008a264e8299cb077ef2e868
Spasidar Hadzhipasev <shadjipasev@gmail.com>
2025-05-03 10:58:56 +0300

    feat: flow.controller error handling.

--- src/controllers/flow.controller.ts
+++ src/controllers/flow.controller.ts
@@ -9,43 +9,51 @@ import {
   getCurrentBlock,
   setCurrentBlock,
 } from "../services/flow.service";
+import { InternalServerError } from "restify-errors";
 
 export const flowController = async (message: string) => {
-  let current = getCurrentBlock();
-  let config = getConfig();
+  try {
+    let current = getCurrentBlock();
+    let config = getConfig();
 
-  await addMessageToConversation({
-    role: ChatRole.User,
-    message,
-    currentBlock: current,
-  });
+    await addMessageToConversation({
+      role: ChatRole.User,
+      message,
+      currentBlock: current,
+    });
 
-  //   console.log("Before WaitForResponse Type", current.type);
-  //   console.log("Before WaitForResponse Id", current.id);
+    //   console.log("Before WaitForResponse Type", current.type);
+    //   console.log("Before WaitForResponse Id", current.id);
 
-  if (current.type === BlockTypes.WaitForResponse) {
-    console.log("Wait for response");
-    setCurrentBlock(current.next);
-    current = getCurrentBlock();
-  }
+    if (current.type === BlockTypes.WaitForResponse) {
+      console.log("Wait for response");
+      setCurrentBlock(current.next);
+      current = getCurrentBlock();
+    }
 
-  //   console.log("Before DetectIntent Type", current.type);
-  //   console.log("Before DetectIntent Id", current.id);
+    //   console.log("Before DetectIntent Type", current.type);
+    //   console.log("Before DetectIntent Id", current.id);
 
-  if (current.type === BlockTypes.DetectIntent) {
-    const intendedReply = await intentDetection(config, message, current);
-    sendMessageToClient(intendedReply.content, current);
-    setCurrentBlock(intendedReply.next);
-    current = getCurrentBlock();
-  }
+    if (current.type === BlockTypes.DetectIntent) {
+      const intendedReply = await intentDetection(config, message, current);
+      sendMessageToClient(intendedReply.content, current);
+      setCurrentBlock(intendedReply.next);
+      current = getCurrentBlock();
+    }
 
-  //   console.log("Before WriteMessage Type", current.type);
-  //   console.log("Before WriteMessage Id", current.id);
-  if (current.type === BlockTypes.WriteMessage) {
-    sendMessageToClient(current.content, current);
-    setCurrentBlock(current.next);
+    //   console.log("Before WriteMessage Type", current.type);
+    //   console.log("Before WriteMessage Id", current.id);
+    if (current.type === BlockTypes.WriteMessage) {
+      sendMessageToClient(current.content, current);
+      setCurrentBlock(current.next);
 
-    // console.log("After WriteMessage Id", current.id);
-    // console.log("After WriteMessage NEXT", current.next);
+      // console.log("After WriteMessage Id", current.id);
+      // console.log("After WriteMessage NEXT", current.next);
+    }
+  } catch (error) {
+    throw new InternalServerError({
+      message: "Chatbot stopped working unexpectedly",
+      cause: error,
+    });
   }
 };
