8ee8de0166faf0ca6f8a314d908978d9b6de5081
Spasidar Hadzhipasev <shadjipasev@gmail.com>
2025-05-04 09:45:55 +0300

    feat: Testing if initial message is send

--- tests/socket.controller.test.ts
+++ tests/socket.controller.test.ts
@@ -2,6 +2,7 @@ import { Server as SocketIoServer } from "socket.io";
 import * as ioc from "socket.io-client";
 import * as restify from "restify";
 import { createApp } from "../src/server";
+import { IBlock, IChatConfig } from "src/models/types/config.interface";
 
 jest.setTimeout(20000);
 
@@ -30,6 +31,30 @@ describe("Socket Controller", () => {
         expect(message).toBe("Hey");
       });
       done();
+      io.on("connection", (mySocket) => {
+        expect(mySocket).toBeDefined();
+      });
+    });
+  });
+
+  describe("should send initial message", () => {
+    it("should send content of starting block", async () => {
+      const mockConfig: IChatConfig = {
+        blocks: <IBlock[]>[
+          {
+            id: "start_block",
+            type: "write_message",
+            content: "Hello!",
+            next: "wait_for_response",
+          },
+        ],
+        startBlock: "start_block",
+      };
+
+      io.emit("initial_message", mockConfig.blocks[0].content);
+      clientSocket.once("initial_message", (message) => {
+        expect(message).toBe(mockConfig.blocks[0].content);
+      });
       io.on("connection", (mySocket) => {
         expect(mySocket).toBeDefined();
       });
